7dd836cba5128276fdf49f550cf806e7f3cce853
ARKye03 <rafa2003sanchez@gmail.com>
2023-05-06 15:32:11 -0500

    Commit 0.2

--- MoogleEngine/MASE.cs
+++ MoogleEngine/MASE.cs
@@ -8,6 +8,6 @@ namespace MoogleEngine{
 public class Engine{
     //Create a function that receives a word or multiple words and put them in an array
     static string? UserInput;
-    static string path = (@"..\content");	
+    static string path = (@"..\content");
 }
 }
