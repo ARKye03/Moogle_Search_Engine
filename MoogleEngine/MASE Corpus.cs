6afa1563eca7e5f78866d09f6b651b1eb229875e
ARKye03 <rafa2003sanchez@gmail.com>
2023-05-07 15:17:32 -0500

    0.2.5

--- MoogleEngine/MASE Corpus.cs
+++ MoogleEngine/MASE Corpus.cs
@@ -1,28 +1,34 @@
 namespace MoogleEngine;
-
-
-
 public class Corpus{
-    string Path = "";
-
-    //Uno para todos, y todas para el diccionario ^~^
-    public static Dictionary<string, double> GeneralFiler = new Dictionary<string, double>();
-
-    public Dictionary<string, Data> Docs = new Dictionary<string, Data>();
+    string Path = ""; // Directorio content(por lo general)
+    public Dictionary<string, double> GeneralFiler = new Dictionary<string, double>(); //Uno para todos, y todas para el diccionario ^~^
+    public Dictionary<string, Data> Docs = new Dictionary<string, Data>(); // Diccionario que almacena el nombre de cada documento y sus datos
+    
     public Corpus(string path){
         this.Path = path;
-        //GetInfo(Directory.GetFiles(path, ".txt"));
-        BuildGeneralFiler(path);
+        GetInfo(Directory.GetFiles(path, "*.txt"));
+        //BuildGeneralFiler(path);
+        IDF();
+        WW(); //WeightWord
     }
-
-    /*static void GetInfo(string[] files){
+    private void WW(){
+        foreach (var par in Docs){
+            par.Value.Peso(GeneralFiler);
+        }
+    }  
+    private void IDF(){
+        foreach (var par in GeneralFiler){
+            GeneralFiler[par.Key] = IDF(par.Key);
+        }
+    }
+    private void GetInfo(string[] files){
         for (int i = 0; i < files.Length; i++){
             BuildGeneralFiler(files[i], i);
         }
-    }*/
+    }
 
     //Calcular el Tf de todas las palabras al inicio del pograma
-    static void TF(string[] words){
+    /*private void TF(string[] words){
         //Diccionario que almacena el valor tf de cada palabra
         Dictionary<string, double> tf = new Dictionary<string, double>();
         foreach (string word in words)
@@ -36,13 +42,9 @@ public class Corpus{
         foreach (string word in tf.Keys.ToList()){
             tf[word] = tf[word] / totalWords;
         }
-    }
+    }*/
 
-
-
-
-
-    static void BuildGeneralFiler(string path){ //Add all the words of all files in the path
+    /*static void BuildGeneralFiler(string path){ //Add all the words of all files in the path
         // Leer todos los archivos del directorio
         foreach (string archivo in Directory.GetFiles(path)){
             // Leer todas las palabras del archivo y agregarlas al GeneralFiler
@@ -54,16 +56,84 @@ public class Corpus{
                 else
                     GeneralFiler[palabra]++;
             }
-        }
-    }
-    double IDF(string palabra) // obvio lo que hace esto no?... no
+        }*
+    }*/
+    /*double IDF(string palabra) // obvio lo que hace esto no?... no
     {
-        int suma = 0;
+        int count = 0;
         foreach (var par in Docs)
         {
             if (!par.Value.vocabulario.ContainsKey(palabra)) continue;
-            suma++;
+            count++;
         }
-        return Math.Log10((double)Docs.Count / (double)suma);
+        return Math.Log10((double)Docs.Count / (double)count);
+    }*/
+    //Function that calculates the IDF of a word in a document
+    double IDF(string word){
+        int count = 0;
+        foreach (var par in Docs){
+            if (!par.Value.vocabulario.ContainsKey(word)) continue;
+            count++;
+        }
+        return Math.Log10((double)Docs.Count / (double)count);
     }
+    private void BuildGeneralFiler(string filename, int index){   
+        var data = new Data();
+        var count = 0; // Count of each word's index
+        var text = File.ReadAllText(Directory.GetFiles(Path, "*.txt")[index]).ToLower();
+        var words = text.Split(new char[] { ' ', ',', '.', ';', '?', '!', '¿', '¡', ':', '"' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var word in words){
+            if (word.Length == 1 && Char.IsPunctuation(word[0])) continue;
+            // Add word to document vocabulary with its index
+            if (!data.vocabulario.ContainsKey(word)){
+                data.vocabulario.Add(word, new List<int>());
+                data.pesos.Add(word, 0);
+            
+                if (!GeneralFiler.ContainsKey(word)){
+                GeneralFiler.Add(word, 0);
+                }
+            }
+            data.vocabulario[word].Add(count);
+        
+            if (data.vocabulario[word].Count > data.MaxWordAppereance){
+                data.MaxWordAppereance = data.vocabulario[word].Count;
+            }
+            count++;
+        }
+    Docs.Add(filename, data);
 }
+
+}
+
+
+/*private void BuildGeneralFiler(string nombre, int i){   
+        Data data = new Data();
+        // Index count of each word
+        int count = 0; 
+        // Splitting with signos de puntuacion
+
+        string txt = File.ReadAllText(Directory.GetFiles(Path, "*.txt")[i]).ToLower();
+        string[] palabras = txt.Split(new char[] { ' ', ',', '.', ';', '?', '!', '¿', '¡', ':', '"' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string word in palabras)
+        {
+            if (word.Length == 1 && Char.IsPunctuation(word[0])) continue;
+        // aqui voy guardando cada una de las palabras en el vocabulario del documento con sus indices
+            if (!data.vocabulario.ContainsKey(word))
+            {
+                data.vocabulario.Add(word, new List<int>());
+                data.pesos.Add(word, 0);
+                if (!GeneralFiler.ContainsKey(word))
+                {
+                    GeneralFiler.Add(word, 0);
+                }
+            }
+            data.vocabulario[word].Add(count);
+            if (data.vocabulario[word].Count > data.MaxWordAppereance)
+            {
+                data.MaxWordAppereance = data.vocabulario[word].Count;
+            }
+            count++;
+        }
+        Docs.Add(nombre, data);
+    }*/
